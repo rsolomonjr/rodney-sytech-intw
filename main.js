a54ddb6e9c9042124374f15249e9e226e7c83107
rsolomonjr <rodneysolomonjr@gmail.com>
2018-01-09 02:14:25 -0500

    jQuery uploaded first

--- main.js
+++ main.js
@@ -1,23 +1,55 @@
 // Please complete the below exercises preferrably using JQuery
 
 // 1. Make each item's paragraph collapse/expand when the title is clicked
-
-
-
-
+  $('.title').on("click", function(){
+ 	$(this).siblings('.description').slideToggle("slow"); 
+ })
 
 // 2. Remove each item when we click on the closing X
 
-
-
+$('.close').on("click", function(){
+	$(this).closest('.item').remove();
+})
 
 
 // 3. Create a new item using the input field
 //    The title of the new item has to match the input value
 //    The new item should also have the same properties (collapse/expand and close) as the other items
 
+jQuery.fn.outerHTML = function() {
+   return jQuery('<div />').append(this.eq(0).clone()).html();
+};
 
 
+$('.add').click(function(e){
+	e.preventDefault(); 
+	$('.title').unbind(); 
+	var newHElem; 
+	value = $('#newtitle').val(); 
+	if(!value){ /* if there is no value in the input */ 
+		
+		var matched = $('.item'); 
+		
+		var newHElem = 'Title ' + (matched.length + 1);
+		$('.item-list').append($('.item:last').outerHTML()); 
+		$('.item:last h1').html(newHElem); 
+
+		
+	} else{ /* else there is a value */ 
+		var newHElem = value; 
+		$('.item-list').append($('.item:last').outerHTML()); 
+		$('.item:last h1').html(newHElem); 
+
+	}		
+
+	$('.title').on("click", function(){
+		 	$(this).siblings('.description').slideToggle("slow"); 
+		 })
+
+		$('.close').on("click", function(){
+			$(this).closest('.item').remove();
+		})
+	})
 
 //    Optional: 
 //    If the user clicks "Add New" and the input is empty, 
